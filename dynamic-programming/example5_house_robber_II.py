7ee940eb27265e10c7b9660fd7cb5da63bd17e61
adrianhsu <adrianhsu@adrianhsudeMacBook-Air.local>
2026-01-04 13:14:42 -0800

    add

--- dynamic-programming/example5_house_robber_II.py
+++ dynamic-programming/example5_house_robber_II.py
@@ -13,6 +13,10 @@ Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2)
 Example 2:
 Input: nums = [1,2,3,1]
 Output: 4
+
+Hint: cannot rob both 0 or n - 1
+Ie.,
+you can only rob either 0 (and no n-1) or rob n - 1 (and no 0)
 """
 
 class Solution:
@@ -40,6 +44,57 @@ class Solution:
         # Scenario 1: Rob houses 0 to n-2 (exclude last house)
         # Scenario 2: Rob houses 1 to n-1 (exclude first house)
         return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
+    
+    # def rob(self, nums: List[int]) -> int:
+    #     if not nums:
+    #         return 0
+    #     elif len(nums) == 1:
+    #         return nums[0]
+        
+    #     n = len(nums)
+    #     dp1 = [0] * len(nums)
+    #     dp2 = [0] * len(nums)
+
+    #     # Case 1: don't rob [0], and do rob [n - 1]
+    #     dp1[0] = 0
+    #     dp1[1] = nums[1]
+    #     for i in range(2, n):
+    #         dp1[i] = max(dp1[i - 1], dp1[i - 2] + nums[i])
+
+            
+    #     # Case 2: do rob [0], and don't rob [n - 1]
+    #     dp2[0] = nums[0]
+    #     dp2[1] = max(nums[0], nums[1])
+    #     dp2[n- 1] = 0
+    #     for i in range(2, n - 1):
+    #         dp2[i] = max(dp2[i - 1], dp2[i - 2] + nums[i])
+
+    #     # Case 3: don't rob both [0] and [n - 1]? doesnt make sense
+
+    #     return max(dp1[n - 1], dp2[n - 2])
+
+    # def rob(self, nums: List[int]) -> int:
+    #     if not nums:
+    #         return 0
+    #     if len(nums) == 1:
+    #         return nums[0]
+        
+    #     def rob_simple(house_list):
+    #         if not house_list: 
+    #             return 0
+            
+    #         dp = [0] * len(house_list)
+    #         dp[0] = house_list[0]
+            
+    #         if len(house_list) > 1:
+    #             dp[1] = max(house_list[0], house_list[1])
+            
+    #         for i in range(2, len(house_list)):
+    #             dp[i] = max(dp[i-1], dp[i-2] + house_list[i])
+                
+    #         return dp[-1]
+
+    #     return max(rob_simple(nums[:-1]), rob_simple(nums[1:]))
 
 # Test cases
 def test_rob_ii():
